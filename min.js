82e2b628c07be10bb1c543b054d065b44b9b3bdf
Volt0303 <volute@gmail.com>
2025-09-20 15:43:51 -0700

    feat:new

---
+++ min.js
@@ -0,0 +1,12 @@
+const min = () =>{
+    return (
+        <>
+            <div>
+                <h1>Hello world!</h1>
+                <p>hello ttaro!! where are u ?</p>
+            </div>
+        </>
+    )
+}
+
+export default min
